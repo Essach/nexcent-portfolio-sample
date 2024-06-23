d99a7ebd9819d321faca63613640b97c641fa416
Adrian <adrian.wozny@interia.pl>
2024-06-23 17:02:33 +0200

    feat. responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,54 @@
-import { Button, Flex, Image, Text } from "@chakra-ui/react";
+import { Button, Drawer, DrawerContent, DrawerOverlay, Flex, Image, Text, useDisclosure } from "@chakra-ui/react";
 import logo from '../assets/images/Logo.png'
+import menu from '../assets/icons/menu.png';
+import { useRef } from "react";
+import logoWhite from '../assets/images/LogoWhite.png';
+import { Link } from "react-scroll";
+
 const Navbar = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = useRef()
+
     return (
-        <Flex px={['18px', '36px', '72px', '144px']}  bg={'brandSilver'} py='22px' alignItems={'center'} justifyContent={'space-between'}>
-            <Image src={logo} _hover={{cursor:'pointer'}} />
-            <Flex gap={['10px', '25px', '25px', '50px']} display={['none','none','none','flex', 'flex', 'flex']}>
-                <Text fontSize='md' fontWeight='medium' color='secondary' _hover={{cursor: 'pointer'}}>
-                    Home
-                </Text>
-                <Text fontSize={'md'} fontWeight='medium' color='secondary' _hover={{cursor: 'pointer'}}>
-                    Service
-                </Text>
-                <Text fontSize={'md'} fontWeight='medium' color='secondary' _hover={{cursor: 'pointer'}}>
-                    Feature
-                </Text>
-                <Text fontSize={'md'} fontWeight='medium' color='secondary' _hover={{cursor: 'pointer'}}>
-                    Product
-                </Text>
-                <Text fontSize={'md'} fontWeight='medium' color='secondary' _hover={{cursor: 'pointer'}}>
-                    Testimonial
-                </Text>
-                <Text fontSize={'md'} fontWeight='medium' color='secondary' _hover={{cursor: 'pointer'}}>
-                    FAQ
-                </Text>
+        <>
+            <Flex px={['18px', '36px', '72px', '144px']} bg={'brandSilver'} py='22px' alignItems={'center'} justifyContent={'space-between'}>
+                <Flex gap={'20px'}>
+                    <Image src={menu} _hover={{ cursor: 'pointer' }} display={['block', 'block', 'none']} ref={btnRef} onClick={onOpen}/>
+                    <Image src={logo} _hover={{cursor:'pointer'}} />
+                </Flex>
+                <Flex gap={['10px', '25px', '25px', '50px']} display={['none', 'none', 'none', 'flex', 'flex', 'flex']}>
+                    {['Home', 'Service', 'Feature', 'Product', 'Testimonial', 'FAQ'].map(element => (<Link key={element} to={element} smooth={true} duration={500} offset={-100}>
+                        <Text _hover={{ cursor:'pointer'}} fontSize='md' fontWeight='medium' color='secondary'>
+                        {element}
+                        </Text>
+                    </Link>))}
+                </Flex>
+                <Flex>
+                    <Button variant='tertiary' size='md'>Login</Button>
+                    <Button variant='primary' size='md'>Sign up</Button>
+                </Flex>
             </Flex>
-            <Flex>
-                <Button variant='tertiary' size='md'>Login</Button>
-                <Button variant='primary' size='md'>Sign up</Button>
-            </Flex>
-        </Flex>
+            <Drawer
+                isOpen={isOpen}
+                placement='left'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+                display={['block', 'block', 'none']}
+            >
+                <DrawerOverlay />
+                <DrawerContent bg='secondary' display={'flex'} flexDirection={'column'} p='35px 100px 35px 30px' gap={'50px'}>
+                    <Image src={logoWhite} />
+                    <Flex gap={['10px', '25px', '25px', '50px']} direction={'column'} pl='20px'>
+                        {['Home', 'Service', 'Feature', 'Product', 'Testimonial', 'FAQ'].map(element => (<Link key={element} to={element} smooth={true} duration={500} offset={-100} onClick={onClose}>
+                            <Text fontSize='2xl' fontWeight='medium' color='white' _hover={{ cursor: 'pointer' }}>
+                                {element}
+                            </Text>
+                    </Link>))}
+                    
+                </Flex>
+                </DrawerContent>
+            </Drawer>
+        </>
     );
 }
 
